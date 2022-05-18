5d1ce8ea114a213cf7a0c65cbd8b840d66c5809c
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-05-18 09:17:08 -0700

    build header

--- src/Header.js
+++ src/Header.js
@@ -7,7 +7,7 @@ function Header() {
     <div className='header'>
         <h1>Header</h1>
         <div className="header__left">
-            <img src="" alt="" />
+            <img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" alt="" />
 
             <div className="header__search">
                 <SearchIcon />

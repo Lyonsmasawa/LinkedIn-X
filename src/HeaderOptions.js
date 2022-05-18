f9b60ad72e3e64271cef0df93f17588f77113945
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-05-18 10:00:36 -0700

    Avatar material

--- src/HeaderOptions.js
+++ src/HeaderOptions.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import './HeaderOptions.css'
-import Avatar from 'react-avatar';
+// import Avatar from 'react-avatar';
+import {Avatar} from "@material-ui/core"
+
 
 function HeaderOptions({ title, Icon, avatar }) {
   return (
     <div className='headerOption'>
         {Icon && <Icon className='headerOption__icon'/>}
-        {avatar && (<Avatar className="headerOption__icon" src={avatar}/>)}
+        {avatar && <Avatar className="headerOption__icon" src={avatar}/>}
         <h3 className='headerOption__title'>{ title }</h3>
     </div>
   )

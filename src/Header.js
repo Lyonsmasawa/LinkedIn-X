7720a2fea83810c96bc85354ef1d97404fb12e84
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-05-18 09:18:06 -0700

    build header

--- src/Header.js
+++ src/Header.js
@@ -5,7 +5,6 @@ import SearchIcon from '@material-ui/icons/Search';
 function Header() {
   return (
     <div className='header'>
-        <h1>Header</h1>
         <div className="header__left">
             <img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" alt="" />
 

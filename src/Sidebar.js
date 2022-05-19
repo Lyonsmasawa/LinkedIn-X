0f1be8bc5c633b8c5acbe33fa7acf6622d344961
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-05-18 21:19:45 -0700

    build sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,9 +1,15 @@
+import { Avatar } from '@material-ui/core'
 import './Sidebar.css'
 
 function Sidebar(){
     return (
         <div className="sidebar">
-            
+            <div className="sidebar__top">
+                <img src="" alt="" />
+                <Avatar />
+                <h2>Lyons Masawa</h2>
+                <h4>lyonsmasawa@gmail.com</h4>
+            </div>
         </div>
     )
 }

b3b0aba64ef9425876b3c4702367ad4fb11a8ae7
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-05-19 05:21:53 -0700

    dynamic data

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -16,8 +16,8 @@ function Sidebar(){
     return (
         <div className="sidebar">
             <div className="sidebar__top">
-                <img src="" alt="" />
-                {Avatar && <Avatar src={user.photoURL} />}
+                <img src="https://images.unsplash.com/photo-1493976040374-85c8e12f0c0e?ixid=MnwxMTI1OHwwfDF8cmFuZG9tfHx8fHx8fHx8MTY1MjcxNzk0Ng&ixlib=rb-1.2.1&q=85&w=1920" alt="" />
+                {user.photoURL != null ? <Avatar src={user.photoURL}/> : <Avatar className="sidebar__avatar" >{user.email[0]}</Avatar>}
                 <h2>{user.displayName}</h2>
                 <h4>{user.email}</h4>
             </div>

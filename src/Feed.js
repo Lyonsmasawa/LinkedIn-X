79ea901984b5f9355262d1042133ce71661d5b2b
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-05-18 23:21:55 -0700

    Post props

--- src/Feed.js
+++ src/Feed.js
@@ -22,7 +22,7 @@ function Feed() {
                 </div>
             </div>
             <div className="feed__post">
-                <Post />
+                <Post name="Lyons Masawa" description="this is it" message="Lorem ipsum Lorem Ipsum Lorem Ipsum Lorem"/>
             </div>
 
 

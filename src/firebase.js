579af8331def4f678ca19f1cc92c38343d386cde
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-05-19 01:04:09 -0700

    integrate firebase

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase'
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyA5yQrPnfe8yXsEC62R0tDsI0THHZ1fCg8",
+    authDomain: "linkedin-clone-x.firebaseapp.com",
+    projectId: "linkedin-clone-x",
+    storageBucket: "linkedin-clone-x.appspot.com",
+    messagingSenderId: "905859546316",
+    appId: "1:905859546316:web:6f3309f91ed81545e55669",
+    measurementId: "G-Z9P03R3NPR"
+  };
+
+  const firebaseApp = firebase.initilizeApp(firebaseConfig)
+  const db = firebaseApp.firestore()
+  const auth = firebase.auth()
+
+  export {db, auth};

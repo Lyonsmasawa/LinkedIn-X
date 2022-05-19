44d9934c33f713542f0cb801b338703df1479b81
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-05-19 04:06:57 -0700

    build login

--- src/Login.js
+++ src/Login.js
@@ -19,10 +19,10 @@ function Login() {
         <img src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c528.png" alt="" />
 
         <form action="">
-            <input type="text" placeholder='Full name (required if registering)' />
+            <input type="text" placeholder='Full name (required if registering)' value={name} onChange={e => setName(e.target.value)} />
             <input type="text" placeholder='Profile picture (optional)' />
             <input type="email" placeholder='Email' value={email} onChange={e => setEmail(e.target.value)}/>
-            <input type="password" placeholder='Password' />
+            <input type="password" placeholder='Password' value={password} onChange={e => setPassword(e.target.value)}/>
             <button type='submit' onClick={loginToApp}>Sign in</button>
         </form>
 

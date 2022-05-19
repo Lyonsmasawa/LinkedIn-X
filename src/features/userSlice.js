6920a0ac7947f240796d2ff6110772409f143d48
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-05-19 04:38:25 -0700

    login functionality

--- src/features/userSlice.js
+++ src/features/userSlice.js
@@ -6,7 +6,7 @@ const initialState = {
 }; 
 
 export const userSlice = createSlice({
-  name: 'counter',
+  name: 'user',
   initialState,
   // The `reducers` field lets us define reducers and generate associated actions
   reducers: {

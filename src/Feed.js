8c682233df3c4fe1380149bb2e03be53fc4e6b13
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-05-18 22:31:22 -0700

    style feed section

--- src/Feed.js
+++ src/Feed.js
@@ -1,9 +1,18 @@
 import './Feed.css'
+import { Create } from '@material-ui/icons'
 
 function Feed() {
     return (
         <div className="feed">
-
+            <div className="feed__inputContainer">
+                <div className="feed__input">
+                    <Create />
+                    <form action="">
+                        <input type="text" />
+                        <button type='submit'>Send</button>
+                    </form>
+                </div>
+            </div>
         </div>
     )
 }

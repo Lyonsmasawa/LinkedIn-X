8e1ee9e2b2c936e96864b9596f905a11f5c22819
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-05-18 23:14:58 -0700

    post section

--- src/Post.js
+++ src/Post.js
@@ -1,9 +1,18 @@
+import { Avatar } from '@material-ui/core'
 import React from 'react'
 import './Post.css'
 
-function Post() {
+function Post(props) {
   return (
-    <div>Post</div>
+    <div className='post'>
+        <div className="post__header">
+            <Avatar />
+            <div className="post__info">
+                <h2>Lyons Masawa</h2>
+                <p>Description</p>
+            </div>
+        </div>
+    </div>
   )
 }
 

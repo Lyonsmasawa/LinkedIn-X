5667a510615e4905f6ac4b61f67fda4a3fad364b
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-05-19 03:12:32 -0700

    build login

--- src/Login.js
+++ src/Login.js
@@ -2,6 +2,9 @@ import React from 'react'
 import './Login.css'
 
 function Login() {
+
+    const register = () => {}
+
   return (
     <div className='login'>
         <img src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c528.png" alt="" />
@@ -11,7 +14,12 @@ function Login() {
             <input type="text" placeholder='Profile picture (optional)' />
             <input type="email" placeholder='Email' />
             <input type="password" placeholder='Password' />
+            <button type='submit'>Sign in</button>
         </form>
+
+        <p>Not a member?
+            <span className='login__register' onClick={register}>Register now</span>
+        </p>
     </div>
   )
 }

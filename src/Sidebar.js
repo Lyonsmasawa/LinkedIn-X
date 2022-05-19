3051e6da5a46c54eda8790b03831a55d381d104d
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-05-18 21:25:32 -0700

    build sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -10,6 +10,22 @@ function Sidebar(){
                 <h2>Lyons Masawa</h2>
                 <h4>lyonsmasawa@gmail.com</h4>
             </div>
+
+            <div className="sidebar__stats">
+                <div className="sidebar__stat">
+                    <p>Who viewed your profile</p>
+                    <p className="sidebar__statNumber">1010</p>
+                </div>
+
+                <div className="sidebar__stat">
+                    <p>Views on post</p>
+                    <p className="sidebar__statNumber">1010</p>
+                </div>
+            </div>
+
+            <div className="sidebar__bottom">
+                <p>Recent</p>
+            </div>
         </div>
     )
 }
